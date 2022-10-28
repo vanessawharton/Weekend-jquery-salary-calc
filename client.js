22b050dfa68f835773022084e9d3034a0c3c093d
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-28 17:31:50 -0500

    Added initial functions

--- client.js
+++ client.js
@@ -1,42 +1,114 @@
-// # Weekend Challenge: jQuery Salary Calculator
-// Create an application that records employee salaries and adds salaries up to report monthly costs. 
 
-// ## Topics Covered
-// - JavaScript
-// - jQuery - Selectors, append, and event handling
+// A 'Submit' button should collect the form information, store the information to calculate monthly costs, 
+// append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs 
+// and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
 
-// ## Assignment
-
-// The application should have an input form that collects _employee first name, last name, ID number, job title, annual salary_.
-
-// A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
-
-// Create a delete button that removes an employee from the DOM. For Base mode, it does **not** need to remove that Employee's salary from the reported total.
-
-// ### Files Provided
-// No files have been provided (just instructions.md and a readme.md). Instead of forking and cloning this repo, please choose "Use This Template" (green button) and name your new repo "weekend-jquery-salary-calculator" and clone down from there. Make sure to commit regularily!
-
-// ### Wireframe
-
-// ![Wireframe](salary-calc-wireframe.png)
+// Create a delete button that removes an employee from the DOM. 
+// For Base mode, it does **not** need to remove that Employee's salary from the reported total.
 
 // ## Stretch Mode
-
 // Add styling or extra functionality that fits with the theme of this assignment.
+// Once the employee is deleted, update the _Total Monthly Cost_ section on the page to reflect the employee's removal. 
+// _HINT:_ You will need to figure out which employee was removed, in order to subtract their salary from the total. 
+// Consider using `.text()` as a getter, or look into jQuery's `.data()` function. This is tricky! 
 
-// Once the employee is deleted, update the _Total Monthly Cost_ section on the page to reflect the employee's removal. _HINT:_ You will need to figure out which employee was removed, in order to subtract their salary from the total. Consider using `.text()` as a getter, or look into jQuery's `.data()` function. This is tricky! 
+$(document).ready(readyNow);
 
-// ## Reminder About Modes
+let employeeList = [];
+let annualTotalValue = 0;
 
-// Above, we introduced the concept of levels of difficulty. "Mode" is how we will typically refer to each level. Below is a brief explanation of
+// function calculateGarageValue (price, array){
+//   let sum = 0;
+//   array.forEach(car.carPrice); {
+//     sum += car.carPrice[price] ?? 0;
+//   };
+//   return sum;
+// };
 
-// * what to expect when attempting each mode
-// * if they are required or not
+function readyNow() {
+    console.log("DOM is loaded!");
+    $('#addEmpBtn').on('click', addEmp);
+    $('#empTable').on('click', '.delete-btn', removeEmp);
+    render();
+} // end readyNow
 
-// Mode | Description
-// --- | ---
-// Base | required
-// Stretch | optional, stretches your understanding and may require additional research
+function checkInputs(){
+    let formComplete = $("#empIdIput").length;
+    // ADD THE REST OF THE INPUTS
 
-// ## Assignment Submission
-// Check in your repo, then turn in your work via the Prime Academy Assignment Application at [http://primeacademy.io](http://primeacademy.io), as usual and don't hesitate to hit up the Slack channel as needed!
+    if (formComplete !== 0){
+        return true
+    } else
+        //ERROR MESSAGE: NEED TO COMPLETE ALL FIELDS OF FORM
+        return false
+} // end checkInputs
+
+function addEmp() {
+    checkInputs();
+    console.log('Attempting to add employee');
+
+    if (checkInputs === true){
+        let emp = {
+            empFirstName: $('#empFirstNameInput').val(),
+            empLastName: $('#empLastNameInput').val(),
+            empId: $('#empIdInput').val(),
+            empTitle: $('#empTitleInput').val(),
+            empAnnSalary: $('#empAnnualSalaryInput').val()
+        };
+    employees.push(emp);
+    console.log(employees);
+
+    render();
+    
+    $('#empFirstNameInput').val("");
+    $('#empLastNameInput').val("");
+    $('#empIdInput').val("");
+    $('#empTitleInput').val("");
+    $('#empAnnualSalaryInput').val("");
+    }
+} // end addEmp
+
+function render() {
+    if (employeeList.length !== 0){
+        $('#empTable').empty();
+
+    for (let emp of employeeList){
+        $('#empTable').append(`
+            <tr>
+                <td>${car.carImg}</td>
+                <td>${car.carYear}</td>
+                <td>${car.carMake}</td>
+                <td>${car.carModel}</td>
+                <td>${car.carPrice}</td>
+                <td>    
+                    <button class="delete-btn">Delete</button>
+                </td>
+            </tr>
+        `);
+    }
+    }
+} // end render
+
+function removeEmp(){ 
+    console.log('in removeEmp');
+
+    let empToRemove = $(this)
+        .parent()
+        .siblings()
+        .last()
+        .val();
+    console.log(empToRemove);
+
+    let newEmployeeList= [];
+
+    for (let emp of employeeList){
+        if (emp.empId !== empToRemove){
+            newEmployeeList.push(emp);
+        }
+    }
+
+    employeeList = newEmployeeList;
+    console.log('New Employee List is:', newEmployeeList);
+
+    render();
+} // end removeEmp
